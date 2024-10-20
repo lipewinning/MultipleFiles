a3f992312af95e94694561da0902753e00093491
lipewinning <felipealvesdesousa@gmail.com>
2024-10-20 08:37:36 +0100

    parametarize-extension

--- project/interfaceModule.py
+++ project/interfaceModule.py
@@ -7,17 +7,18 @@ path = "C:\\Users\\felipe\\Desktop\\study\\python\\MultipleFiles\\project\\r_inp
 
 def zip_file_listener(path):
     while True:
+        print('-->> Checking directory ' + path)
         zipFiles = zipModule.zipFiles(path)
         if not zipFiles:
             dt = datetime.now().strftime("%H:%M:%S")
-            print('Could not find any zip file ' + dt) 
+            print('     Could not find any zip file ' + dt) 
         else:
             for filePath in zipFiles:
                 zipModule.unzip(filePath, path)
                 zipModule.removeFile(filePath)
         
         time.sleep(sleepTime)
-        print('Checking..')
+        
 
 
 def file_name_matches_with_interfaces(file_name, interfaces):
--- project/zipModule.py
+++ project/zipModule.py
@@ -2,12 +2,14 @@ import shutil
 import os
 import glob
 
+fileExtension = "zip"
+
 def unzip(inputFile,destinationFolder):
-        shutil.unpack_archive(inputFile, destinationFolder,"zip")
+        shutil.unpack_archive(inputFile, destinationFolder, fileExtension)
         print('UNZIP: ' + inputFile)
 
 def zipFiles(path):
-    return glob.glob(path + '/*.zip')
+    return glob.glob(path + '/*.' + fileExtension)
 
 def removeFile(filePath):
     if os.path.isfile(filePath):
